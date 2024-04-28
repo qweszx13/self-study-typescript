c92433d8ff0b2fdb7819dfa15170951c4a31e1fd
Kimdonggyu <qweszx33@naver.com>
2024-04-28 14:49:21 +0900

    test:create primitive-types

---
+++ TS/primitive-types.js
@@ -0,0 +1,20 @@
+var myName = 'kim';
+var myAge = 50;
+var married = true;
+var empty = null;
+var unde = undefined;
+var signedMember = ['kim', 'park'];
+var signedMember2 = {
+    member1: 'kim',
+    member2: 'park',
+};
+var myInfo = {
+    name: 'john',
+    age: 25,
+    address: 'korea',
+};
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
---
+++ TS/primitive-types.ts
@@ -0,0 +1,22 @@
+let myName :string = 'kim';
+let myAge :number = 50;
+let married :boolean = true;
+let empty :null = null;
+let unde :undefined = undefined;
+let signedMember :string[] = ['kim','park'];
+let signedMember2 :{ member1 :string, member2 :string } = {
+  member1 : 'kim',
+  member2 : 'park',
+};
+
+let myInfo :{ name :string, age :number, address :string} = {
+  name:'john',
+  age:25,
+  address:'korea',
+}
+
+let project :{ member :string[], days: number, started :boolean} = {
+  member : ['kim', 'park'],
+  days : 30,
+  started : true,
+}

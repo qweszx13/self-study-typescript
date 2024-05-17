7ad6867cd2bb90c578b1993de51bec782251e1e6
qweszx13 <qweszx33@naver.com>
2024-05-17 15:06:12 +0900

    feat:create interface-practice create-time:2024-05-17 15:06

---
+++ TS/interface-practice.js
@@ -0,0 +1,10 @@
+let square = { color: 'red', width: 100 };
+let student = { name: 'kim', score: 20 };
+let teacher = { name: 'kim', age: 20, score: 30 };
+;
+let struct = { brand: 'Samsung', serialNumber: 1360, model: ['TV', 'phone'] };
+let cart = [{ product: "whiper", price: 4000 }, { product: "samdasu", price: 1200 }];
+const plusMinus = {
+    plus: (x, y) => x + y,
+    minus: (x, y) => x - y
+};
